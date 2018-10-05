53218086985381e3dcbe3ae80aa8e2a2a093034b
lenchu <553308706@qq.com>
2018-10-05 11:02:45 +0800

    login

--- controllers/UserC.py
+++ controllers/UserC.py
@@ -1,5 +1,25 @@
-from app import route
+from app import route, render_template
+from flask import request
+from models import User
 
 @route('/user', methods = ['GET'])
 def findUser(): 
     return '<h1>user</user>'
+
+
+@route('/login', methods = ['GET'])
+def login():
+    return render_template('login.html', action = '/login')
+
+
+@route('/login', methods = ['POST'])
+def handleLogin():
+    un = request.form['username']
+    pw = request.form['password']
+    try:
+        user = User.get(User.username==un,
+                        User.password==pw)
+        return user.username
+    except:
+        return 'username and passwor not match'
+
